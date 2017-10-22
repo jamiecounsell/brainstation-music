5ce2ab1e266074c66ff17d7f99a0de61ccd39288
Sandro Nasso <sandro.ordnas@live.ca>
2017-10-21 22:01:13 -0400

    U2 files beautified

--- u2/index.js
+++ u2/index.js
@@ -1,29 +1,29 @@
-$(function() {
+$(function () {
 
     var commentsArray = [];
 
 
-    $( "#commentsTable" ).hide();
+    $("#commentsTable").hide();
 
 
-    $( "img" ).hover(
-        
+    $("img").hover(
+
         function () {
-            $( "img" ).attr("src", "u2.jpg");
-        }, 
-        
+            $("img").attr("src", "u2.jpg");
+        },
+
         function () {
-            $( "img" ).attr("src", "U2-pic.jpg");
+            $("img").attr("src", "U2-pic.jpg");
         }
     );
 
 
     var submitButton = $("#submitButton");
-    submitButton.click(function() {
+    submitButton.click(function () {
 
-        var firstName   = $("#firstName");
-        var lastName    = $("#lastName");
-        var commentBox  = $("#commentBox");
+        var firstName = $("#firstName");
+        var lastName = $("#lastName");
+        var commentBox = $("#commentBox");
 
         var message = "";
         if (firstName.val() == "" && lastName.val() == "") {
@@ -53,8 +53,11 @@ $(function() {
 
         var comment = commentBox.val();
 
-        commentsArray.push({"firstName": firstName.val(), "lastName": lastName.val(),
-            "comment": commentBox.val()});
+        commentsArray.push({
+            "firstName": firstName.val(),
+            "lastName": lastName.val(),
+            "comment": commentBox.val()
+        });
 
         buildCommentsTable();
     });
@@ -67,29 +70,29 @@ $(function() {
 
 
         for (var i in commentsArray) {
-            
+
             var commentDict = commentsArray[i];
-            
+
             var tableRef = document.getElementById('commentsTable').getElementsByTagName('tbody')[0];
 
             var newRow = tableRef.insertRow(tableRef.rows.length);
-            
+
 
             var newCell1 = newRow.insertCell(0);
             var newCell2 = newRow.insertCell(1);
-            
-                                                            
-            var name    = document.createTextNode(commentDict.firstName + " " + commentDict.lastName);
+
+
+            var name = document.createTextNode(commentDict.firstName + " " + commentDict.lastName);
             var comment = document.createTextNode(commentDict.comment);
 
             newCell1.appendChild(name);
             newCell2.appendChild(comment);
         }
 
-        
-        $( "#commentsTable" ).show();
+
+        $("#commentsTable").show();
 
         document.getElementById("commentsTable").scrollIntoView();
     }
-    
+
 });
